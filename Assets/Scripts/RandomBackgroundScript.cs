bfbf4f89a30c050a92138eec77880eec209d15ea
Reaver <c0de187nig@gmail.com>
2021-12-15 04:37:40 +0200

    - Small update
    - Small update

--- Assets/Scripts/RandomBackgroundScript.cs
+++ Assets/Scripts/RandomBackgroundScript.cs
@@ -1,16 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RandomBackgroundScript : MonoBehaviour {
 
+public class RandomBackgroundScript : MonoBehaviour {
+	public RandomBackgroundScript RandomBackgroundScriptMaster;
+	public int PictureValue = 0;
+	public bool SetMaster = false;
 	// Use this for initialization
 	void Start () {
-        (GetComponent<Renderer>() as SpriteRenderer).sprite = Backgrounds[Random.Range(0, Backgrounds.Length)];
+		if (RandomBackgroundScriptMaster != null)
+		{
+			PictureValue = Random.Range(0, Backgrounds.Length);
+			(GetComponent<Renderer>() as SpriteRenderer).sprite = Backgrounds[PictureValue];
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if(!SetMaster && RandomBackgroundScriptMaster != null)
+        {
+			(GetComponent<Renderer>() as SpriteRenderer).sprite = Backgrounds[RandomBackgroundScriptMaster.PictureValue];
+			SetMaster = true;
+		}
 	}
 
     public Sprite[] Backgrounds;
